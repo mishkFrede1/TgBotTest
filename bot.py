15f1780769b07c083e455ddba5dc78bbf8cec5ac
VadimDvache <damirmukanov8@gmail.com>
2024-10-21 22:58:14 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,24 +20,4 @@ async def main():
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
-import asyncio
-import os
-from aiogram import Bot, Dispatcher
-from dotenv import load_dotenv
-from aiogram.client.bot import DefaultBotProperties
-
-from handlers import base_commands
-
-async def main():
-    load_dotenv()
-    bot = Bot(os.getenv('TOKEN'), default=DefaultBotProperties(parse_mode="HTML"))
-    dp = Dispatcher(bot=bot)
-    
-    dp.include_routers(
-        base_commands.router
-    )
-
-    await dp.start_polling(bot)
-
-if __name__ == "__main__":
     asyncio.run(main())
